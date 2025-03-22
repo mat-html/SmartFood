2ca77d2965a1df814c82b2c2443f5f32619db0ad
0Hammad01 <yasinhammad320@gmail.com>
2025-03-22 16:40:52 +0200

    Update Login.tsx

--- SmartFood/src/pages/Login.tsx
+++ SmartFood/src/pages/Login.tsx
@@ -1,7 +1,12 @@
-function Login() {
+import "../css/login.css"
+const Login = () => {
   return (
-  <form>
-     
+    
+
+
+    
+    <form>
+      
       <div data-mdb-input-init className="form-outline mb-4">
         <input type="email" id="form2Example1" className="form-control" />
         <label className="form-label" htmlFor="form2Example1">
@@ -9,7 +14,7 @@ function Login() {
         </label>
       </div>
 
-     
+      
       <div data-mdb-input-init className="form-outline mb-4">
         <input type="password" id="form2Example2" className="form-control" />
         <label className="form-label" htmlFor="form2Example2">
@@ -17,10 +22,10 @@ function Login() {
         </label>
       </div>
 
-     
+      
       <div className="row mb-4">
         <div className="col d-flex justify-content-center">
-          {/* Checkbox */}
+         
           <div className="form-check">
             <input
               className="form-check-input"
@@ -36,7 +41,7 @@ function Login() {
         </div>
 
         <div className="col">
-          
+        
           <a href="#!">Forgot password?</a>
         </div>
       </div>
@@ -54,47 +59,19 @@ function Login() {
      
       <div className="text-center">
         <p>
-          Not a member? <a href="#!">Register</a>
+          Not a member? <a href="">Sign Up</a>
         </p>
-        <p>or sign up with:</p>
-        <button
-          type="button"
-          data-mdb-button-init
-          data-mdb-ripple-init
-          className="btn btn-link btn-floating mx-1"
-        >
-          <i className="fab fa-facebook-f"></i>
-        </button>
+       
+        
+        
 
-        <button
-          type="button"
-          data-mdb-button-init
-          data-mdb-ripple-init
-          className="btn btn-link btn-floating mx-1"
-        >
-          <i className="fab fa-google"></i>
-        </button>
+        
+         
 
-        <button
-          type="button"
-          data-mdb-button-init
-          data-mdb-ripple-init
-          className="btn btn-link btn-floating mx-1"
-        >
-          <i className="fab fa-twitter"></i>
-        </button>
-
-        <button
-          type="button"
-          data-mdb-button-init
-          data-mdb-ripple-init
-          className="btn btn-link btn-floating mx-1"
-        >
-          <i className="fab fa-github"></i>
-        </button>
+      
       </div>
     </form>
   );
-}
+};
 
 export default Login;
