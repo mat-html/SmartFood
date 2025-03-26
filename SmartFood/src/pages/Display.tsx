ed9cad3ac35e879f9cbc130921e85eeafb4e9bee
Adrian <if24b114@technikum-wien.at>
2025-03-26 05:45:10 +0100

    Impressum, Display und Hilfe wurde gemacht, aber Css fehlt, kann leider nicht sehen wie es aussehen könnte, aber wird dann weitergemacht. Grundstein wurde mal gesetzt

--- SmartFood/src/pages/Display.tsx
+++ SmartFood/src/pages/Display.tsx
@@ -1,7 +1,20 @@
+const produkt = [{
+  id:
+  beschreibung:
+  preis
+  pname
+  bild
+}
+];
+
 function Display() {
   return (
     <main>
-      <h1>Display</h1>
+      <h1>{produkt.pname}</h1>
+      <img src={produkt.bild} alt={produkt.name} style={{ width: '300px' }} />
+      <p>{produkt.beschreibung}</p>
+      <p><strong>Preis:</strong> {produkt.preis}</p>
+      <button>In den Warenkorb</button>
     </main>
   );
 }
