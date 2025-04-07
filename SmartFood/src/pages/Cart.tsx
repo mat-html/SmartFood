3856ec412841263f2848f0d9acccef47f5732d09
Matic Jovan <matic.jovan219@gmail.com>
2025-04-08 00:17:58 +0200

    cart page v0.0.1

--- SmartFood/src/pages/Cart.tsx
+++ SmartFood/src/pages/Cart.tsx
@@ -2,6 +2,41 @@ function Cart() {
   return (
     <main>
       <h1>Cart</h1>
+      <div className="row g-5">
+        <div className="col-md-5 col-lg-4 order-md-last">
+          <h4 className="d-flex justify-content-between align-items-center mb-3">
+            <span className="text-primary">Your cart</span>
+            <span className="badge bg-primary rounded-pill">3</span>
+          </h4>
+          <ul className="list-group mb-3">
+            <li className="list-group-item d-flex justify-content-between lh-sm">
+              <div>
+                <h6 className="my-0">Product name</h6>
+                <small className="text-body-secondary">Brief description</small>
+              </div>
+              <span className="text-body-secondary">$12</span>
+            </li>
+            <li className="list-group-item d-flex justify-content-between lh-sm">
+              <div>
+                <h6 className="my-0">Second product</h6>
+                <small className="text-body-secondary">Brief description</small>
+              </div>
+              <span className="text-body-secondary">$8</span>
+            </li>
+            <li className="list-group-item d-flex justify-content-between lh-sm">
+              <div>
+                <h6 className="my-0">Third item</h6>
+                <small className="text-body-secondary">Brief description</small>
+              </div>
+              <span className="text-body-secondary">$5</span>
+            </li>
+            <li className="list-group-item d-flex justify-content-between">
+              <span>Total (USD)</span>
+              <strong>$20</strong>
+            </li>
+          </ul>
+        </div>
+      </div>
     </main>
   );
 }
