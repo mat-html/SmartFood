699ae9730efa919f15ac470e6c0accf8d64f025c
Matic Jovan <matic.jovan219@gmail.com>
2025-04-08 00:54:19 +0200

    cart and checkout pages v0.1.0

--- SmartFood/src/pages/Cart.tsx
+++ SmartFood/src/pages/Cart.tsx
@@ -1,12 +1,16 @@
+import "../css/cart.css";
+
 function Cart() {
   return (
     <main>
       <h1>Cart</h1>
-      <div className="row g-5">
-        <div className="col-md-5 col-lg-4 order-md-last">
+      <div className="row justify-content-center">
+        <div className="col-md-12 col-lg-8">
           <h4 className="d-flex justify-content-between align-items-center mb-3">
-            <span className="text-primary">Your cart</span>
-            <span className="badge bg-primary rounded-pill">3</span>
+            <span className="textCart">Your cart</span>
+            <span className="badge rounded-pill" id="numOfItems">
+              3
+            </span>
           </h4>
           <ul className="list-group mb-3">
             <li className="list-group-item d-flex justify-content-between lh-sm">
@@ -35,6 +39,7 @@ function Cart() {
               <strong>$20</strong>
             </li>
           </ul>
+          <button id="orderBtn">Order now</button>
         </div>
       </div>
     </main>
--- SmartFood/src/pages/Menu.tsx
+++ SmartFood/src/pages/Menu.tsx
@@ -1,16 +1,16 @@
-import "../css/menu.css";
 import MenuItem from "../components/MenuItem";
 
 function Menu() {
-  const menuItems = [
-    { name: "Pizza", imageUrl: "/img/pizza.jpg" },
-    { name: "Burger", imageUrl: "/img/burger.jpg" },
+  const menuItems = [{name: "Pizza", imageUrl: "/img/pizza.png"}, {name: "Burger", imageUrl: "/img/burger.png"},
+                     {name: "Fish and chips", imageUrl: "/img/fish-and-chips.png"}, {name: "Gyros", imageUrl: "/img/gyros.png"},
+                     {name: "Lasagna", imageUrl: "/img/lasagna.png"}, {name: "Salad", imageUrl: "/img/salad.png"},
+                     {name: "Spaghetti", imageUrl: "/img/spaghetti.png"}
   ];
 
   return (
     <main>
       <h1>Menu</h1>
-      <ul className="d-flex">
+      <ul className="d-flex flex-wrap">
         {menuItems.map((item, index) => (
           <li key={index}>
             <MenuItem name={item.name} imageUrl={item.imageUrl} />
