dc773dfc139ee3fbeaa9b36bbba332fe3e0becde
Barnabas Matrai <matrai.barnabas1@gmail.com>
2025-04-08 00:10:37 +0200

    add more menu items

--- SmartFood/src/components/MenuItem.tsx
+++ SmartFood/src/components/MenuItem.tsx
@@ -7,10 +7,10 @@ type MenuItemProps = {
 
 function MenuItem ({ name, imageUrl }: MenuItemProps) {
     return (
-        <div className="col">
+        <div className="col m-2">
             <div className="card shadow-sm">
-                <svg className="menu-item-svg bd-placeholder-img card-img-top" width="100%" xmlns="http://www.w3.org/2000/svg" role="img" 
-                aria-label="Placeholder: Thumbnail" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid slice">
+                <svg className="menu-item-svg bd-placeholder-img card-img-top" xmlns="http://www.w3.org/2000/svg" role="img" 
+                aria-label="Placeholder: Thumbnail" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid slice" width="18vw" height="18vw">
                     <image href={imageUrl} width="100%" height="100%" />
                 </svg>
                 <div className="card-body">
